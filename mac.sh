a16008228f0233916df8396f6e7da065028871b9
Piotr Kubowicz <piotr.kubowicz@gmail.com>
2021-12-14 08:13:18 +0100

    Support Mac 'say'

---
+++ mac.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env zsh
+
+show_help () {
+    echo "Usage: ./ssml.sh [ tel | tel-fast | tel-zh | to1000 | year | zh ] [ voice ]" >&2
+    exit 0
+}
+
+[[ -z "$1" ]] && show_help
+[[ "$1" == "--help" ]] && show_help
+
+WORKDIR=$(dirname $0)
+JS="$WORKDIR/$1"
+[[ ! $JS =~ "\.js$" ]] && JS="$JS.js"
+[[ ! -f $JS ]] && show_help
+
+VOICE=$2
+
+SPEECH=$(node $JS | sed 's/<[^>]*>//g')
+echo $SPEECH > /dev/stderr
+echo $SPEECH | say -v $VOICE -
