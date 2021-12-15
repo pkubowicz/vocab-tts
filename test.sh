92ae92a39b210597c1fa68b62f656cdea6077afa
Piotr Kubowicz <piotr.kubowicz@gmail.com>
2021-12-15 21:40:51 +0100

    Test also ssml.sh, reduce test verbosity

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 
+set -e
 for i (*.js); do
     OUT=$(echo foo | node $i)
     if [[ $? -ne 0 ]]; then
@@ -8,6 +9,8 @@ for i (*.js); do
     fi
 done
 
-echo "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n" | ./chinese-input.sh > /dev/null
+echo "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n" | ./chinese-input.sh > /dev/null 2>&1
+./ssml.sh tel > /dev/null 2>&1
+./ssml.sh tel.js > /dev/null 2>&1
 
 echo ok
